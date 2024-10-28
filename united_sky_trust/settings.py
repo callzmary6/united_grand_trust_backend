b6146f33d8c139fd33a9ea74db7622716732e26c
Godson Jerry <jegoisgs66@gmail.com>
2024-10-28 18:35:33 +0100

    update

--- united_sky_trust/settings.py
+++ united_sky_trust/settings.py
@@ -155,9 +155,9 @@ cloudinary.config(
 )
 
 # EMAIL CONFIGURATION
-EMAIL_USE_TLS= True
+EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.titan.email'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'info@unitedgrandtrust.com'
 EMAIL_HOST_PASSWORD = 'Info@Unitedgrandtrust123'
 
